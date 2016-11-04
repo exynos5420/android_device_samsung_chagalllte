edbcc6ff208a6eb7acea0eb5a414a783826c71af
Sean Hoyt <seanhoyt963@gmail.com>
2016-11-04 00:06:07 -0500

    Use Exynos5420-common
    Change-Id: I639941746b9f0e320b00d76beb6bba74beed205d

--- aicp.mk
+++ aicp.mk
@@ -1,10 +1,10 @@
-# Inherit some common AICP stuff.
+$(call inherit-product, device/samsung/chagalllte/full_chagalllte.mk)
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/aicp/configs/common_tablet.mk)
 
-# Inherit telephony stuff
-$(call inherit-product, vendor/aicp/configs/telephony.mk)
-
-$(call inherit-product, device/samsung/chagalllte/full_chagalllte.mk)
+# Inherit more cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/telephony.mk)
 
 PRODUCT_NAME := aicp_chagalllte
 PRODUCT_DEVICE := chagalllte
@@ -13,9 +13,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T805 \
     PRODUCT_NAME=chagalllte \
     PRODUCT_DEVICE=chagalllte \
-    TARGET_DEVICE=chagalllte \
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1600
--include vendor/aicp/configs/bootanimation.mk
+    TARGET_DEVICE=chagalllte
