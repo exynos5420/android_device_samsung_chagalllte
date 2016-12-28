3a324d41379433d1251d7aed13f07bf3cc1dc947
Sinisa Sehovic (Thermskin)) <sinisa.sehovic@gmail.com>
2016-12-29 00:36:15 +0100

    chagalllte: shim gpsd
    Change-Id: I1da7bb4c147d615284c3d3620752280d14853e2d

---
+++ libshim/libshim.c
@@ -0,0 +1,4 @@
+// Android N exports
+// android::SensorManager::getSensorList(android::Sensor const* const**) const
+extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE();
+extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(){}
