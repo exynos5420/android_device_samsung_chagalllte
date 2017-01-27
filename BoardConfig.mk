29b2339a9d033b42dba9b0ee8d2d3b8d0a50c95b
Christopher N. Hesse <raymanfx@gmail.com>
2017-01-27 23:33:06 +0100

    cmhw: Add MDNIE RGB tuning capabilities
    Change-Id: If3c2baf64cea2429bf34e54fd8164c1c2e32e94c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,5 +60,8 @@ BOARD_SEPOLICY_DIRS += device/samsung/chagalllte/sepolicy
 # custom additions to updater-script
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/chagalllte/releasetools/ota_from_target_files
 
+# Cyanogen Hardware
+BOARD_HARDWARE_CLASS := $(COMMON_PATH)/cmhw
+
 # Inherit from exynos5420-common
 include device/samsung/exynos5420-common/BoardConfigCommon.mk
