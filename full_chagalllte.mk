7fda99d18a9a3e07521a0f3e21e31399251dd4e2
Sean Hoyt <seanhoyt963@gmail.com>
2017-02-14 15:16:37 -0500

    Remove ril for now
    Change-Id: Icdae56adcde0d6c352c200beed82cac0ff67e8eb

---
+++ full_chagalllte.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+
+# Inherit more AICP stuff.
+$(call inherit-product, vendor/aicp/configs/telephony.mk)
+
+# Inherit from chagalllte device
+$(call inherit-product, device/samsung/chagalllte/device.mk)
+
+PRODUCT_NAME := aicp_chagalllte
+PRODUCT_DEVICE := chagalllte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-T805
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=SM-T805 \
+    PRODUCT_NAME=chagalllte \
+    PRODUCT_DEVICE=chagalllte \
+    TARGET_DEVICE=chagalllte
