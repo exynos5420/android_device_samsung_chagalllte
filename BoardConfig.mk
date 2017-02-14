a2803a00efa04cd646a4adaae1587454ffc97e40
Sean Hoyt <seanhoyt963@gmail.com>
2017-02-14 15:16:37 -0500

    Remove ril
    Change-Id: Icdae56adcde0d6c352c200beed82cac0ff67e8eb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,14 +20,6 @@ LOCAL_PATH := device/samsung/chagalllte
 BOARD_VENDOR := samsung
 TARGET_SOC := exynos5420
 
-# RIL
-BOARD_PROVIDES_LIBRIL := true
-BOARD_MODEM_TYPE := xmm7260
-BOARD_MODEM_NEEDS_VIDEO_CALL_FIELD := true
-
-# RIL.java overwrite
-BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
-
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
--- device.mk
+++ device.mk
@@ -52,6 +52,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
+PRODUCT_PACKAGES += \
+    libxml2
+
 # Shim
 PRODUCT_PACKAGES += \
     libshim
@@ -62,11 +65,8 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+#    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
     fstab.universal5420 \
@@ -74,17 +74,8 @@ PRODUCT_PACKAGES += \
     init.baseband.rc \
     ueventd.universal5420.rc
 
-# Radio
-PRODUCT_PACKAGES += \
-    libril \
-    librilutils \
-    rild \
-    libxml2 \
-    libprotobuf-cpp-full \
-    modemloader
-
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carrier=unknown
+    ro.carrier=wifi-only
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/chagalllte/sepolicy
