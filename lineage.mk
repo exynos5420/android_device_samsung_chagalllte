acdbff52fdc847e31386617624b467892c861326
Sean Hoyt <seanhoyt963@gmail.com>
2017-02-14 15:16:37 -0500

    Remove ril
    Change-Id: Icdae56adcde0d6c352c200beed82cac0ff67e8eb

--- lineage.mk
+++ lineage.mk
@@ -1,14 +1,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 
 $(call inherit-product, device/samsung/chagalllte/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
-# Inherit more LineageOS stuff.
-$(call inherit-product, vendor/cm/config/telephony.mk)
 
 PRODUCT_NAME := lineage_chagalllte
 PRODUCT_DEVICE := chagalllte
