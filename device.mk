44227b5c55f4fa9e7b05652a59c73321b8ee597e
Basil Gello <vasek.gello@gmail.com>
2017-07-14 07:53:33 +0300

    device.mk: dont re-initialize PRODUCT_PROPERTY_OVERRIDES but append them
    Change-Id: I98e4cfd5ae71cef4c1253a71aeab7264c0077998

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
-PRODUCT_PROPERTY_OVERRIDES := \
+PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.force_voice_config=wide \
     audio_hal.disable_two_mic=true
 
