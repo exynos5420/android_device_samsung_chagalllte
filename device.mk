4ae5fbadbc505e18947f0db4c81241d644213686
Sinisa Sehovic (Thermskin)) <sinisa.sehovic@gmail.com>
2018-02-06 21:00:55 +0100

    Cleanup again!
    Change-Id: I1e1c271428f0ae3440b499e147542ec10eef7bf6

--- device.mk
+++ device.mk
@@ -38,12 +38,6 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1600
 
-# Fingerprint
-#PRODUCT_PACKAGES += \
-#    fingerprintd \
-#    fingerprint.universal5420 \
-#    ValidityService
-
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
@@ -67,7 +61,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
-#    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
     fstab.universal5420 \
@@ -87,27 +80,27 @@ PRODUCT_PACKAGES += \
     sensors.universal5420
 
 # Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=288m \
-    dalvik.vm.heapsize=768m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    dalvik.vm.heapstartsize=8m \
+#    dalvik.vm.heapgrowthlimit=288m \
+#    dalvik.vm.heapsize=768m \
+#    dalvik.vm.heaptargetutilization=0.75 \
+#    dalvik.vm.heapminfree=512k \
+#    dalvik.vm.heapmaxfree=8m
 
 # HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.hwui.texture_cache_size=72 \
+#    ro.hwui.layer_cache_size=48 \
+#    ro.hwui.path_cache_size=32 \
+#    ro.hwui.gradient_cache_size=1 \
+#    ro.hwui.drop_shadow_cache_size=6 \
+#    ro.hwui.r_buffer_cache_size=8 \
+#    ro.hwui.texture_cache_flushrate=0.4 \
+#    ro.hwui.text_small_cache_width=1024 \
+#    ro.hwui.text_small_cache_height=1024 \
+#    ro.hwui.text_large_cache_width=2048 \
+#    ro.hwui.text_large_cache_height=1024
 
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/chagalllte/chagalllte-vendor.mk)
