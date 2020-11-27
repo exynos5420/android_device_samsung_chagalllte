820027caa33cb38e7e8487bde2ded98d090eb196
Ian Hope <ian.hope666@gmail.com>
2020-11-28 10:55:10 +1100

    shim for gpsd updates
    Change-Id: I3960a1142a6f54bd64e9bccd81d6b3ac2d56cfda

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
+LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils libsensor
 LOCAL_SRC_FILES := libshim.cpp
 LOCAL_MODULE := libshim
 LOCAL_MODULE_TAGS := optional
